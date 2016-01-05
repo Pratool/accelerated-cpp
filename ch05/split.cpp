adf484d51dd67239eb3261ad8d49cc704ff6662b
Pratool Gadtaula <pratool.gadtaula@students.olin.edu>
2016-01-04 21:21:42 -0500

    Added split exercise from chapter 5

---
+++ ch05/split.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cctype>
+
+using namespace std;
+
+vector<string> split(const string& s) {
+    vector<string> ret;
+    typedef string::size_type string_size;
+    string_size i = 0;
+
+    while (i != s.size()) {
+        while (i != s.size() && isspace(s[i]))
+            ++i;
+        string_size j = i;
+        while (j != s.size() && !isspace(s[j]))
+            j++;
+        if (i != j) {
+            ret.push_back(s.substr(i, j-i));
+            i = j;
+        }
+    }
+    return ret;
+}
+
+int main() {
+    string input_str = "Mary had a little lamb";
+    vector<string> strs = split(input_str);
+
+    vector<string>::size_type i;
+
+    for (i = 0; i != strs.size(); ++i)
+        cout << strs[i] << endl;
+
+    return 0;
+}
